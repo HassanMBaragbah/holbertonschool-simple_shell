81ba591f6d0559e41bbd797a7ef34d26e9cbf40f
Hassan <7soon721@gmail.com>
2026-08-23 15:06:18 +0300

    Add main.c file for task 1

---
+++ main.c
@@ -0,0 +1,16 @@
+/**
+ * main - entry point for the simple shell
+ * @ac: argument count
+ * @av: argument vector
+ * @env: environment variables
+ *
+ * Return: 0 on success, or exit code on failure
+ */
+int main(int argc, char **argv, char **env)
+{
+	(void)argc;
+	(void)argv;
+	(void)env;
+
+	return (0);
+}
